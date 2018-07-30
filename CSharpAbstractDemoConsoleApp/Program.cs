f9670c48a4ff3435f40b2a5dbfe9e265f11d5656
programpacific <programpacific@users.noreply.github.com>
2018-07-29 21:36:46 -0600

    Initial commit.

--- CSharpAbstractDemoConsoleApp/Program.cs
+++ CSharpAbstractDemoConsoleApp/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+
+//1. Create an abstract class called Person with two properties: string firstName and string lastName.
+//2. Give it the method SayName().
+//3. Create another class called Employee and have it inherit from the Person class.
+//4. Implement the SayName() method inside of the Employee class.
+//5. Inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student". Call the SayName() method on the object.
+
+
+
+
 namespace CSharpAbstractDemoConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            employee.SayName();
+
         }
     }
 }
